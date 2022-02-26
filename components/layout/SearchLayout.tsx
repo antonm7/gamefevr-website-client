1a89f5c8253ce054e59e2eac4c8113b80c85b731
antonm7 <migolkoanton@gmail.com>
2022-02-26 16:09:59 +0200

    changed from side navbar to top navbar

--- components/layout/SearchLayout.tsx
+++ components/layout/SearchLayout.tsx
@@ -1,9 +1,9 @@
+import Navbar from "../Navbar";
+
 export default function SearchLayout({children}:any) {
     return (
         <div className="flex flex-col">
-            <div className="h-20 fixed w-full bg-red-200">
-                SideBar
-            </div>
+            <Navbar />
             <main className="pt-20">{children}</main>
         </div>
     )
