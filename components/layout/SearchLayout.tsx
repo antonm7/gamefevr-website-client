1c3a1adea745c29c3b482930c5c2fad7559a2507
antonm7 <migolkoanton@gmail.com>
2022-02-26 14:18:57 +0200

    removed sidebar to be top navbar

--- components/layout/SearchLayout.tsx
+++ components/layout/SearchLayout.tsx
@@ -1,10 +1,10 @@
 export default function SearchLayout({children}:any) {
     return (
-        <div className="flex flex-row">
-            <div className="h-screen fixed w-72 bg-red-200">
+        <div className="flex flex-col">
+            <div className="h-20 fixed w-full bg-red-200">
                 SideBar
             </div>
-            <main className="pl-72">{children}</main>
+            <main className="pt-20">{children}</main>
         </div>
     )
 }
