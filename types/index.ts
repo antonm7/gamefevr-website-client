6ffffec2caadf307a4ea1f2d89336a90c010c7e7
antonm7 <migolkoanton@gmail.com>
2022-02-26 16:07:25 +0200

    modified some types based on api fetch from rawg like the detailed game (used on game page), and ShortGame used on small game box component

--- types/index.ts
+++ types/index.ts
@@ -1,41 +1,82 @@
-export type Genre = {
+export type ElementDescription = {
     id: number;
+    games_count:number,
+    image_background: string;
+    slug: string;
     name: string;
 }
 
 export type Platform = {
-    id: number;
-    name: string;
+    platform:{
+        id: number,
+        games_count:number,
+        image_background: string,
+        name: string,
+        slug: string,
+        year_end: number,
+        year_start: number,
+    };
+    released_at:string;
+    requirements: any
 }
 
-export type Tag = {
-    id:number;
-    name:string;
+export type ParentPlatform = {
+    platform: {
+        id: number,
+        name: string,
+        slug: string
+    }
 }
 
-export type ScreenShot = {
+export type Short_Screenshot = {
     id:number;
     image:string;
 }
 
+export type Screenshot = {
+    count:number;
+    next:any;
+    previous:any;
+    results:Short_Screenshot[]
+}
+
 export type Store = {
     id:number;
-    store: {
-        domain:string,
-        id:number,
-        name:string,
-    }
+    game_id:number;
+    store_id:number;
+    url:string;
 }
 
 export type ShortGame = {
     id: number;
     name: string;
-    genres: Genre[];
+    genres: ElementDescription[];
     platforms: Platform[];
     released:string;
     slug:string;
-    tags:Tag[];
-    short_screenshots:ScreenShot[];
-    stores:Store[];
+    tags:ElementDescription[];
+    short_screenshots:Short_Screenshot[];
     background_image:string;
+}
+
+export type DetailedGame = {
+    id: number;
+    name: string;
+    released:string;
+    background_image:string;
+    description:string;
+    genres: ElementDescription[];
+    developers: ElementDescription[];
+    parent_platforms: ParentPlatform[];
+    platforms: Platform[];
+    stores: {
+        count:number,
+        next:any,
+        previous:any,
+        results:Store[]
+    };
+    publishers: ElementDescription[];
+    screenshots: Screenshot[];
+    tags:ElementDescription[];
+    website:string;
 }
