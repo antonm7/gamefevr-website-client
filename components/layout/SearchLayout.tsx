067db20157eca9ff0467ffaf950fa1051fd6c76f
antonm7 <migolkoanton@gmail.com>
2022-03-10 15:12:05 +0200

    layout style fix

--- components/layout/SearchLayout.tsx
+++ components/layout/SearchLayout.tsx
@@ -2,7 +2,7 @@ import Navbar from "../Navbar";
 
 export default function SearchLayout({children}:any) {
     return (
-        <div className="flex flex-col">
+        <div>
             <Navbar />
             <main className="pt-20">{children}</main>
         </div>
