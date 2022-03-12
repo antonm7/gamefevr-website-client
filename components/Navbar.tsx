72bc13263aa737066dd4285564e5c908750c2595
antonm7 <migolkoanton@gmail.com>
2022-03-12 19:30:39 +0200

    navbar change based on useSession

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,29 +1,86 @@
 import Link from "next/link";
 import Image from 'next/image'
+import { useRouter } from "next/router";
+import SmallSearchInput from "./common/SmallSearchInput";
+import { useSession } from "next-auth/react";
 export default function Navbar() {
-    return (
-        <div className="absolute h-20 w-full">
-            <div className="h-full w-full flex justify-between pt-6 px-36">
-                <div >
-                    <Image src={'/images/Logo.svg'} height={32} width={130} alt="Logo"/>
-                </div>
-                <div className="flex pt-0">
+    const router = useRouter()
+
+    const {data:session} = useSession()
+
+    const DynamicSession = () => {
+        if(session) {
+            return (
+                <div style={{width:130,cursor:'pointer'}}>
                     <Link href="/">
-                        <p className="text-white font-regular text-sm">Home</p>
-                    </Link>
-                    <Link href="/">
-                        <p className="text-white mx-8 font-regular text-sm">Explore</p>
-                    </Link>
-                    <Link href="/">
-                        <p className="text-white font-regular text-sm">Reviews</p>
+                        <div style={{borderWidth:0.5,borderColor:'#ef626d'}} className="rounded-lg w-full h-11">
+                            <p style={{lineHeight:'2.75rem'}} className="text-white font-regular text-sm text-center">Profile</p>
+                        </div>
                     </Link>
                 </div>
-                <div style={{width:130}}>
+            )}
+        else {
+            return (
+                <div style={{width:130,cursor:'pointer'}}>
                     <Link href="/">
-                        <p className="text-white font-semibold text-lg float-right">Profile</p>
+                        <div style={{borderWidth:0.5,borderColor:'#ef626d'}} className="rounded-lg w-full h-11">
+                            <p style={{lineHeight:'2.75rem'}} className="text-white font-regular text-sm text-center">Register</p>
+                        </div>
                     </Link>
+                </div>
+            )}
+    }   
+
+    if(router.route === '/') {
+        return (
+            <div className="absolute h-20 w-full">
+                <div className="h-full w-full flex justify-between pt-4 px-44 items-center">
+                    <div style={{marginTop:3}}>
+                        <Image src={'/images/Logo.svg'} height={32} width={130} alt="Logo"/>
+                    </div>
+                    <div className="flex pt-0">
+                        <Link href="/">
+                            <p className="text-white font-regular cursor-pointer text-xs">Home</p>
+                        </Link>
+                        <Link href="/">
+                            <p className="text-white mx-8 font-regular cursor-pointer text-xs">Explore</p>
+                        </Link>
+                        <Link href="/">
+                            <p className="text-white font-regular cursor-pointer text-xs">Reviews</p>
+                        </Link>
+                    </div>
+                    <DynamicSession />
                 </div>
             </div>
-        </div>
-    )
+        )
+    } else {
+        return (
+            <div className="absolute h-20 w-full">
+                <div className="h-full w-full flex justify-between pt-4 px-44 items-center">
+                    <div className="flex items-center">
+                        <div style={{marginTop:3}}>
+                            <Image src={'/images/Logo.svg'} height={32} width={130} alt="Logo"/>
+                        </div>
+                        <div className="flex pl-16">
+                            <Link href="/">
+                                <p className="text-white font-regular cursor-pointer text-xs">Home</p>
+                            </Link>
+                            <Link href="/">
+                                <p className="text-white px-8 font-regular cursor-pointer text-xs">Explore</p>
+                            </Link>
+                            <Link href="/">
+                                <p className="text-white font-regular cursor-pointer text-xs">Reviews</p>
+                            </Link>
+                        </div>
+                    </div>
+                    <div className="flex items-center">
+                        <div>
+                            <SmallSearchInput />
+                        </div>
+                        <DynamicSession />
+                    </div>
+                </div>
+            </div>
+        )
+    }
 }
