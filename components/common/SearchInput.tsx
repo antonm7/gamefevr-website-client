f429c2467ad19390b3542e608311452f6f917c66
antonm7 <migolkoanton@gmail.com>
2022-03-12 19:35:20 +0200

    added access for filters, added v for navbar

--- components/common/SearchInput.tsx
+++ components/common/SearchInput.tsx
@@ -1,10 +1,14 @@
-import { getGames } from "../../lib/functions/dataGames"
+import { faSliders } from "@fortawesome/free-solid-svg-icons"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { useStore } from "../../store"
 
 export default function SearchInput() {
-    getGames()
-    return (
-        <input autoSave="true" autoFocus={true} placeholder="Search..." className="w-700 text-white placeholder-slate-400 outline-0 p-4 h-16 bg-inputBg rounded-lg">
+    const store = useStore()
 
-        </input>
+    return (
+        <div className="flex items-center relative ">
+            <FontAwesomeIcon onClick={() => store.changeFilterVisibility(true)} icon={faSliders} className="absolute h-4 cursor-pointer right-4 text-gray-600"/>
+            <input autoSave="true" autoFocus={true} placeholder="Search..." className="w-700 text-white placeholder-slate-400 outline-0 p-4 h-16 bg-inputBg rounded-lg" />
+        </div>
     )
 }
---
+++ components/common/SmallSearchInput.tsx
@@ -0,0 +1,13 @@
+import { faSliders } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { useStore } from "../../store";
+
+export default function SmallSearchInput() {
+    const store = useStore()
+    return (
+        <div className="flex items-center relative ">
+            <FontAwesomeIcon onClick={() => store.changeFilterVisibility(true)} icon={faSliders} className="absolute h-3 cursor-pointer right-4 text-gray-600"/>
+            <input autoSave="true" placeholder="Search..." className="w-500 text-white text-xs placeholder-slate-400 outline-0 p-4 h-10 bg-inputBg rounded-lg" />
+        </div>
+    )
+}
