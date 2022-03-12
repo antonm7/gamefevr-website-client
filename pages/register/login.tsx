2d8c1be16b5f6dd73d66565eadb8b140714ad14d
antonm7 <migolkoanton@gmail.com>
2022-03-12 19:37:10 +0200

    login page, added todo

--- pages/register/login.tsx
+++ pages/register/login.tsx
@@ -1,3 +1,19 @@
-export default function Login() {
-    
+import { NextPage } from "next"
+import { signIn } from "next-auth/react"
+const Login:NextPage = () => {
+    const signin = () => {
+        //inputs verify
+        //use signIn with 'credentials',
+        //.then((res) => {
+        // setLoading(false)
+        // if(res.error) {
+        //   setLoading(false)
+        //   return setLoginerror('Email or password is invalid')
+    }
+        return (
+        <>
+            LoginPage
+        </>
+    )
 }
+export default Login
