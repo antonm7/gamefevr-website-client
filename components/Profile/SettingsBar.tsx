9d09bebdaf2e8844b357d72f172c06edf921fd72
Anton Migolko <migolkoanton@gmail.com>
2022-06-02 11:20:56 +0300

    visibility animation for settings bar

--- components/Profile/SettingsBar.tsx
+++ components/Profile/SettingsBar.tsx
@@ -1,7 +1,15 @@
 import { useState } from "react";
 import SettingsInput from "./SettingsInput";
 import YellowButton from '../common/YellowButton'
-export default function SettingsBar(props:any) {
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faXmark } from "@fortawesome/free-solid-svg-icons";
+
+interface Props {
+    isOpened: boolean,
+    method: () => void
+}
+
+export default function SettingsBar(props:Props) {
     const [username, setUsername] = useState<string>('')
     const [email, setEmail] = useState<string>('')
     const [password, setPassword] = useState<string>('')
@@ -12,7 +20,12 @@ export default function SettingsBar(props:any) {
     }
 
     return (
-        <div className="h-full fixed z-10 bg-darkIndigo right-0 rounded-2xl p-16" style={{width:'28rem'}}>
+        <div 
+            id="settings_bar" 
+            className={`h-full fixed z-10 bg-darkIndigo right-0 rounded-2xl p-16 ${props.isOpened ? 'opened' : ''}`}
+            onClick={() => props.method()}
+            style={{width:'28rem'}}>
+            <FontAwesomeIcon icon={faXmark} className="absolute cursor-pointer" style={{height:22,color:'#c7c7c7',right:35,top:42}}/>
             <h1 className="text-white font-semibold text-2xl">Account Settings</h1>
             <div className="pt-4 pb-12">
                 <SettingsInput label="Username" placeholder="Enter Your Username" onChange={(e:any) => setUsername(e.target.value)} type="text"/>
--- pages/profile/[id].tsx
+++ pages/profile/[id].tsx
@@ -1,21 +1,28 @@
 import { faGear } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { useState } from 'react'
 import SearchLayout from '../../components/layout/SearchLayout'
 import Favorite from '../../components/Profile/Favorite'
 import Reviews from '../../components/Profile/Reviews'
 import SettingsBar from '../../components/Profile/SettingsBar'
 
 export default function Profile() {
+    const [isOpened, setIsOpened] = useState<boolean>(false)
+
+    const changeVisibleSettings = (value:boolean) => {
+        setIsOpened(value)
+    }
+
     return (
         <SearchLayout>
             <main className="px-44 py-10">
-             <SettingsBar />
+             <SettingsBar isOpened={isOpened} method={() => changeVisibleSettings(false)}/>
                 <div className='flex justify-between items-center'>
                     <h1 className='text-white font-bold text-4xl'>Welcome Anton!</h1>
                     {/* 
                     TODO:Check if it is needed
                     <h2>Need to verify your </h2> */}
-                    <div className='flex items-center'>
+                    <div className='flex items-center' onClick={() => changeVisibleSettings(true)}>
                         <FontAwesomeIcon icon={faGear} className="pr-2 cursor-pointer" style={{color:'#616e7e'}}/>
                         <p className="text-large font-semibold cursor-pointer" style={{color:'#616e7e'}}>Account Settings</p>
                     </div>
