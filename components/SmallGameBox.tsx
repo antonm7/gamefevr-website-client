46de44321f21a2bfc15e940bcbab3bf190e26ff3
Anton Migolko <migolkoanton@gmail.com>
2022-06-09 13:41:52 +0300

    added game consoles

--- components/SmallGameBox.tsx
+++ components/SmallGameBox.tsx
@@ -1,6 +1,7 @@
 import Link from "next/link"
 import Image from 'next/image'
-import { ElementDescription, ShortGame } from "../types"
+import { ElementDescription, ParentPlatform, ShortGame } from "../types"
+
 type Props = {
     game:ShortGame;
     key:number
@@ -8,8 +9,10 @@ type Props = {
 
 export default function SmallGameBox(props:Props) {
     const game = props.game
+
+    console.log(game)
     return (
-        <div className="w-80 h-72 bg-white rounded-lg mx-8 my-4 overflow-hidden" style={{height:'1%'}}>
+        <div className="w-96 h-72 bg-white rounded-lg mx-8 mb-12 overflow-hidden" style={{height:'1%'}}>
             <div  className="bg-image" >
                 <Image quality="1" loading="eager" className="z-0" src={game.background_image} layout="fill" objectFit="cover" />
             </div>
@@ -17,9 +20,12 @@ export default function SmallGameBox(props:Props) {
                 <a href={`/game/${props.game.id}`}>
                     <h1 style={{lineBreak:'anywhere'}} className="font-semibold text-xl whitespace-pre-wrap hover:text-gray-500">{game.name}</h1>
                 </a>
+                <div className="flex flex-row flex-nowrap">
+                    {game.parent_platforms.slice(0, 3).map((platform:any,index:number) => <h2 key={index} className='pr-1 text-sm text-cool-blue'>{platform.platform.name}{index === game.parent_platforms.length - 1 || index === 2 ? '' : ','}</h2>)}
+                </div>
                 <div className="flex flex-row justify-between pt-6">
-                    <div className="flex flex-row flex-nowrap">{game.genres.slice(0, 3).map((genre:ElementDescription,index:number) => <h2 key={index} className={'pr-1'}>{genre.name}{index === game.genres.length - 1 || index === 2 ? '' : ','}</h2>)}</div>
-                    <h2>{game.released.slice(0,4)}</h2>
+                    <div className="flex flex-row flex-nowrap">{game.genres.slice(0, 3).map((genre:ElementDescription,index:number) => <h2 key={index} className='pr-1 text-sm' style={{color:'#919191'}}>{genre.name}{index === game.genres.length - 1 || index === 2 ? '' : ','}</h2>)}</div>
+                    <h2 className='pr-1 text-sm' style={{color:'#919191'}}>{game.released.slice(0,4)}</h2>
                 </div>
             </div>
         </div>
