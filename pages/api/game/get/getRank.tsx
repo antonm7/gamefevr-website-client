69157aa48eaedd9488c8802a6835b1c686ea9d19
Anton Migolko <migolkoanton@gmail.com>
2022-07-01 19:12:08 +0300

    organized endpoints into different folders

---
+++ pages/api/game/get/getRank.tsx
@@ -0,0 +1,25 @@
+import {Request, Response} from 'express';
+import clientPromise from '../../../../lib/functions/mongodb'
+
+export default async function handler(req:Request, res:Response) {
+    if(req.method === 'GET') {
+        try {
+            const query = req.query
+            const client = await clientPromise
+            const db = client.db('gameFevr')
+            
+            const doc = await db.collection('ranks').findOne({userId:query.userId, gameId:query.gameId})
+            
+            if(!doc) {
+                res.status(200).send({error:null,isUserRated:false})
+            } else {
+                res.status(200).send({error:null,isUserRated:doc.value})
+            }
+
+        } catch (e) {
+            console.log('error on getRank',e)
+            return res.status(500).send({erro:'Unexpected Error'})
+        }
+    }
+}
+
