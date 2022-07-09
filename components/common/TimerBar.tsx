1f99fbee84ec6d5dc438a507a343b04a7fd88cdb
Anton Migolko <migolkoanton@gmail.com>
2022-07-09 19:06:32 +0300

    Loading error component

---
+++ components/common/TimerBar.tsx
@@ -0,0 +1,25 @@
+import { useEffect, useState } from "react";
+
+export default function TimerBar(props:any) {
+    const [w, setW] = useState<number>(0);
+
+    useEffect(() => {
+        if(props.start) {
+            setTimeout(() => {
+                if(w > 288) {
+                    setW(0)
+                    return
+                }
+                setW(w + 1);
+        },12)
+    } else {
+        setW(0)
+    }
+}   ,[props.start,w])
+
+    return (
+        <div className="h-1 bg-red-200 absolute " style={{bottom:0,left:0,width:`${w}px`}}>
+            <div></div>
+        </div>
+    )
+}
--- pages/search/index.tsx
+++ pages/search/index.tsx
@@ -4,21 +4,31 @@ import SearchButton from "../../components/common/SearchButton"
 import { useEffect, useState } from "react";
 import { useRouter } from "next/router";
 import axios from "axios";
-import { useStore } from "../../store";
+import { useGlobalError, useStore } from "../../store";
 import Filters from "../../components/Filters";
 import SmallLoader from '../../components/common/SmallLoader'
 import { ShortGame } from "../../types";
+import LoadingError from "../../components/common/LoadingError";
 
 interface Props {
   games: ShortGame[];
+  error:string | null;
 }
 
 export default function Index(props:Props) {
   const [loading, setLoading] = useState<boolean>(false)
+  const changeGlobalErrorVisibility = useGlobalError(store => store.setIsVisible);
+  //2 types of errors
+  const [loadingError, setLoadingError] = useState<boolean>(false)
+  const [noResults, setNoResults] = useState<boolean>(false)
+
   const router = useRouter()
   const store = useStore()
 
   const loadGames = async (cur:number) => {
+    if(loadingError) {
+      setLoadingError(false)
+    }
     try {
       setLoading(true)
       const getData = await axios.post('/api/query/search', {
@@ -29,39 +39,49 @@ export default function Index(props:Props) {
       store.addGames(getData.data.games)
       setLoading(false)
     } catch (e) {
+      setLoading(false)
+      changeGlobalErrorVisibility(true)
       console.log('ERROR',e)
     }
   }
 
-  // useEffect(() => {
-  //   if(!router.isReady) return;
-  //   if(store.games.length === 0) {
-  //     setLoading(true)
-  //     store.clearPage()
-  //     store.clearGames()
-  //     loadGames(1)
-  //   }
-  // },[router.query,router.isReady])
-
   useEffect(() => {
     if(store.games.length === 0) {
+      if(props.error) {
+        setLoadingError(true)
+        return
+      }
+      if(props.games.length === 0) {
+        setNoResults(true)
+        return
+      }
       store.addPage()
       store.addGames(props.games)
     }
   },[props])
 
+
   return (
       <SearchLayout>
         <div>
-        {store.isFilterOn ? <Filters /> : null}
-          <div className="py-10">
-            <div className="flex flex-wrap justify-center">
-              {store.games.map((game:ShortGame,index:number) => <SmallGameBox key={index} game={game}/>)}
+          {store.isFilterOn ? <Filters /> : null}
+          {loadingError ? 
+            <div className="pt-32">
+              <LoadingError mainTitle={'Unexpected Error'} description={"Oops...something went wrong"} button={true} onClick={() => loadGames(1)}/>
+            </div> : 
+            noResults ?
+            <div className="pt-32">
+              <LoadingError mainTitle={"No Results Found"} description={"We couldnt find what you searched..."} /> 
+            </div> :
+            <div className="py-10">
+              <div className="flex flex-wrap justify-center">
+                {store.games.map((game:ShortGame,index:number) => <SmallGameBox key={index} game={game}/>)}
+              </div>
+              <div className='w-24 h-16 rounded-lg m-auto mt-8'>
+                {loading ? <SmallLoader big={false} xCentered={true}/> : <SearchButton text="Load More" onClick={() => loadGames(store.page)}/>}
+              </div>
             </div>
-            <div className='w-24 h-16 rounded-lg m-auto mt-8'>
-              {loading ? <SmallLoader big={false} xCentered={true}/> : <SearchButton text="Load More" onClick={() => loadGames(store.page)}/>}
-            </div>
-          </div>
+          }
         </div>
       </SearchLayout>
     )
@@ -77,53 +97,63 @@ interface Context {
 
 export async function getServerSideProps(context:Context) {
   const {yearRange, genres, consoles } = context.query
-  let games = []
   let filteredString:string = ''
-  if(yearRange || genres || consoles) {
-      if(yearRange) {
-          filteredString = filteredString.concat('', `&dates=${yearRange[0]}-01-01,${yearRange[1]}-12-31`);
-      }
-    //simetimes from the client i get consoles as string, but i need an array
-    //thats why i am checkinf the type of the consoles
-      if(consoles) {
-          if(typeof consoles === 'string') {
-              filteredString = filteredString.concat(`&parent_platforms=${consoles}`)
-          } else {
-              let consolesString:string = ''
-              for(let key in consoles) {
-                  if(parseInt(key) !== consoles.length - 1) {
-                      consolesString = consolesString.concat(`${consoles[key]}`,',')
-                  } else {
-                      consolesString = consolesString.concat(`${consoles[key]}`,'') 
-                  }
-              }   
-              filteredString = filteredString.concat(`&platforms=${consolesString}`)
-          }
-      }
-      if(genres) {
-          if(typeof genres === 'string') {
-              filteredString = filteredString.concat(`&genres=${genres}`)
-          } else {
-              let genresString:string = ''
-              for(let key in genres) {
-                  if(parseInt(key) !== genres.length - 1) {
-                      genresString = genresString.concat(`${genres[key]}`,',')
-                  } else {
-                      genresString = genresString.concat(`${genres[key]}`,'') 
-                  }
-              }   
-              filteredString = filteredString.concat(`&genres=${genresString}`)
-          }
-      } 
-      const getData = await fetch(`https://api.rawg.io/api/games?key=e996863ffbd04374ac0586ec2bcadd55&ordering=-released&page=1&page_size=20${filteredString}`)
-      games = await getData.json()
-  } else {
-      const getData = await fetch(`https://api.rawg.io/api/games?key=e996863ffbd04374ac0586ec2bcadd55&ordering=-released&page=1&page_size=20`)
-      games = await getData.json()
-  }
-  return {
+  let games = []
+  try {
+    if(yearRange || genres || consoles) {
+        if(yearRange) {
+            filteredString = filteredString.concat('', `&dates=${yearRange[0]}-01-01,${yearRange[1]}-12-31`);
+        }
+      //simetimes from the client i get consoles as string, but i need an array
+      //thats why i am checkinf the type of the consoles
+        if(consoles) {
+            if(typeof consoles === 'string') {
+                filteredString = filteredString.concat(`&parent_platforms=${consoles}`)
+            } else {
+                let consolesString:string = ''
+                for(let key in consoles) {
+                    if(parseInt(key) !== consoles.length - 1) {
+                        consolesString = consolesString.concat(`${consoles[key]}`,',')
+                    } else {
+                        consolesString = consolesString.concat(`${consoles[key]}`,'') 
+                    }
+                }   
+                filteredString = filteredString.concat(`&platforms=${consolesString}`)
+            }
+        }
+        if(genres) {
+            if(typeof genres === 'string') {
+                filteredString = filteredString.concat(`&genres=${genres}`)
+            } else {
+                let genresString:string = ''
+                for(let key in genres) {
+                    if(parseInt(key) !== genres.length - 1) {
+                        genresString = genresString.concat(`${genres[key]}`,',')
+                    } else {
+                        genresString = genresString.concat(`${genres[key]}`,'') 
+                    }
+                }   
+                filteredString = filteredString.concat(`&genres=${genresString}`)
+            }
+        } 
+        const getData:any = await axios(`https://api.rawg.io/api/games?key=e996863ffbd04374ac0586ec2bcadd55&ordering=-released&page=1&page_size=20${filteredString}`)
+        games = getData.data.results
+    } else {
+        const getData:any = await axios(`https://api.rawg.io/api/games?key=e996863ffbd04374ac0586ec2bcadd55&ordering=-released&page=1&page_size=20`)
+        games = getData.data.results
+    }
+    return {
       props: {
-          games: games.results,
+        games,
+        error:null
       }
+    }
+  } catch (e) {
+    return {
+        props: {
+            games: [],
+            error:'Error Loading Games'
+        }
+    }
   }
 }
