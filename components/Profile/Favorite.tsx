cf06ef68e72fdfc06acf15e58b56eb6cc0e03b92
Anton Migolko <migolkoanton@gmail.com>
2022-07-09 11:36:33 +0300

    visited and current profile components added

--- components/Profile/Favorite.tsx
+++ components/Profile/Favorite.tsx
@@ -6,34 +6,49 @@ import Image from 'next/image'
 import { Favorite_Type } from '../../types/schema'
 
 interface Props extends Favorite_Type {
-    deleteFavorite: (id:ObjectId | undefined) => void
+    deleteFavorite?: (id:ObjectId | undefined) => void;
+    visited?: boolean;
 }
 
 export default function Favorite(props:Props) {
     const deleteFavorite = async () => {
-        try {
-            const req = await axios.post('/api/game/cancel/favorite/deleteFavorite', {
-                userId:props.userId,
-                gameId:props.gameId,
-                favoriteId:props._id
-            })
-            if(req.status === 200) {
-                props.deleteFavorite(props._id)
-            } else {
-                throw new Error('Unexpected Error')
+        if(props.deleteFavorite) {
+            try {
+                const req = await axios.post('/api/game/cancel/favorite/deleteFavorite', {
+                    userId:props.userId,
+                    gameId:props.gameId,
+                    favoriteId:props._id
+                })
+                if(req.status === 200) {
+                    props.deleteFavorite(props._id)
+                } else {
+                    throw new Error('Unexpected Error')
+                }
+            } catch (e) {
+                console.log(e)
             }
-        } catch (e) {
-            console.log(e)
         }
     }
 
-    return (
-       <div className='relative w-80 h-56 rounded-lg bg-white'>
-            <FontAwesomeIcon className="h-4 absolute z-10 cursor-pointer" icon={faBookmark} style={{color:'#38b6cc',right:20,top:15}} onClick={() => deleteFavorite()}/>
-            <div id="favorite-image">
-                <Image quality="1" loading="eager" objectPosition={'center center'} className="z-0" src={props.game_image} layout="fill" objectFit="cover" />
+    if(props.visited) {
+        return (
+           <div className='relative w-80 h-56 rounded-lg bg-white'>
+                <div id="favorite-image">
+                    <Image quality="1" loading="eager" objectPosition={'center center'} className="z-0" src={props.game_image} layout="fill" objectFit="cover" />
+                </div>
+                <h1 style={{lineBreak:'anywhere'}} className="font-semibold text-lg whitespace-pre-wrap hover:text-gray-500 px-6 py-3">{props.game_name}</h1>
+           </div>
+        )
+    } else {
+        return (
+            <div className='relative w-80 h-56 rounded-lg bg-white'>
+                 <FontAwesomeIcon className="h-4 absolute z-10 cursor-pointer" icon={faBookmark} style={{color:'#38b6cc',right:20,top:15}} onClick={() => deleteFavorite()}/>
+                 <div id="favorite-image">
+                     <Image quality="1" loading="eager" objectPosition={'center center'} className="z-0" src={props.game_image} layout="fill" objectFit="cover" />
+                 </div>
+                 <h1 style={{lineBreak:'anywhere'}} className="font-semibold text-lg whitespace-pre-wrap hover:text-gray-500 px-6 py-3">{props.game_name}</h1>
             </div>
-            <h1 style={{lineBreak:'anywhere'}} className="font-semibold text-lg whitespace-pre-wrap hover:text-gray-500 px-6 py-3">{props.game_name}</h1>
-       </div>
-    )
+         )
+    }
+
 }
--- pages/profile/[id].tsx
+++ pages/profile/[id].tsx
@@ -1,151 +1,67 @@
-import { faGear } from '@fortawesome/free-solid-svg-icons'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { useEffect, useState } from 'react'
-import Slider from 'react-slick'
-import SearchLayout from '../../components/layout/SearchLayout'
-import Favorite from '../../components/Profile/Favorite'
-import Review from '../../components/Profile/Review'
-import SettingsBar from '../../components/Profile/SettingsBar'
 import { getSession } from 'next-auth/react'
 import clientPromise from '../../lib/functions/mongodb'
 import { ObjectId } from 'bson'
 import { Client_User, Favorite_Type, full_user, Review_Type } from '../../types/schema'
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
+import CurrentProfile from '../../components/Profile/CurrentProfile'
+import VisitedProfile from '../../components/Profile/VisitedProfile'
+import { useEffect } from 'react';
 
 interface Props {
     reviews: Review_Type[],
     favorites: Favorite_Type[],
-    user:Client_User
+    user:Client_User,
+    visited:boolean
 }
 
 export default function Profile(props:Props) {
-    const [isOpened, setIsOpened] = useState<boolean>(false)
-    const [user, setUser] = useState<any>(null)
-    const [reviews, setReviews] = useState<Review_Type[]>([])
-    const [favorites, setFavorites] = useState<Favorite_Type[]>([])
-
-    const changeVisibleSettings = (value:boolean) => {
-        setIsOpened(value)
-    }
-
-    const settings = {
-        infinite: false,
-        slidesToShow: props.reviews.length >= 3 ? 3 : props.reviews.length
-    }
-
-    const favoritesSettings = {
-        infinite: false,
-        slidesToShow: props.favorites.length >= 4 ? 4 : props.favorites.length
-    }
-
-    useEffect(() => {
-        setUser(props.user)
-        setReviews(props.reviews)
-        setFavorites(props.favorites)
-    },[props.user])
-
-    const deleteReview = (id:ObjectId | undefined) => {
-        if(id) {
-            const newReviews = reviews.filter((review:Review_Type) => review._id !== id)
-            setReviews(newReviews)
+    if(props.visited) {
+            return (
+                <VisitedProfile 
+                    reviews={props.reviews} 
+                    favorites={props.reviews} 
+                    user={props.user}
+                />
+            )
+        } else {
+            return (
+                <CurrentProfile 
+                    reviews={props.reviews} 
+                    favorites={props.reviews} 
+                    user={props.user}
+                />
+            )
         }
-    }
-
-    const deleteFavorite = (id:ObjectId | undefined) => {
-        if(id) {
-            const newFavorites = favorites.filter((favorite:Favorite_Type) => favorite._id !== id)
-            setFavorites(newFavorites)
-        }
-    }
-
-    if(!user) {
-        return null
-    } else {
-        return <SearchLayout>
-            <main className="px-44 py-10" id="profile_page">
-                <SettingsBar isOpened={isOpened} method={() => changeVisibleSettings(false)}/>
-                <div id="profile_page_welcome" className='flex justify-between items-center'>
-                    <h1 id="welcome_title" className='text-white font-bold text-4xl'>Welcome {user.username}!</h1>
-                    <div className='flex items-center' onClick={() => changeVisibleSettings(true)}>
-                        <FontAwesomeIcon icon={faGear} className="h-4 pr-2 cursor-pointer" style={{color:'#616e7e'}}/>
-                        <p className="text-large font-semibold cursor-pointer" style={{color:'#616e7e'}}>Account Settings</p>
-                    </div>
-                </div>
-                <div className='pt-24'>
-                    <div className='flex items-center justify-between'>
-                        <h1 className='text-white font-bold text-3xl'>Your Reviews</h1>
-                    </div>
-                    <div className='mt-12'>
-                        {reviews.length > 0 ?
-                            <Slider {...settings}>
-                                {reviews.map((review:Review_Type,index:number) => (
-                                    <Review 
-                                        key={index} 
-                                        _id={review._id} 
-                                        likes={review.likes} 
-                                        dislikes={review.dislikes} 
-                                        gameId={review.gameId} 
-                                        userId={review.userId} 
-                                        created_at={review.created_at} 
-                                        text={review.text} 
-                                        rank={review.rank} 
-                                        game_name={review.game_name} 
-                                        game_image={review.game_image}
-                                        deleteReview={(id) => deleteReview(id)} />
-                                ))}
-                            </Slider> :
-                            <div className='text-md text-white font-semibold opacity-30'>You don't have favorite games yet!</div>
-                        }
-                    </div>
-                </div>
-                <div className='pt-14'>
-                    <div className='flex items-center justify-between'>
-                        <h1 className='text-white font-bold text-3xl'>Favorite Games</h1>
-                    </div>
-                    <div className='mt-12'>
-                        {favorites.length > 0 ?
-                            <Slider {...favoritesSettings}> 
-                                {favorites.map((review:Favorite_Type,index:number) => (
-                                    <Favorite 
-                                        _id={review._id}
-                                        key={index} 
-                                        userId={review.userId} 
-                                        created_at={review.created_at}    
-                                        gameId={review.gameId}
-                                        game_name={review.game_name}
-                                        game_image={review.game_image}
-                                        deleteFavorite={(id) => deleteFavorite(id)}
-                                    />
-                                ))}
-                            </Slider> :
-                            <div className='text-md text-white font-semibold opacity-30'>You don't have favorite games yet!</div>
-                        }
-                    </div>
-                </div>
-            </main>
-        </SearchLayout>
-    }
 }
 
 export async function getServerSideProps(context:any) {
     const session = await getSession(context)
 
     try {
+        let user:any, reviews, favorites;
+        const isVisited = context.params.id !== session?.user?.userId
         const client = await clientPromise
         const db = client.db('gameFevr')
-        const user:any = await db.collection('users').findOne({_id:new ObjectId(session?.user?.userId)})
 
-        const reviews = await db.collection('reviews').find({userId:session?.user?.userId}).toArray()
-        const favorites = await db.collection('favorites').find({userId:session?.user?.userId}).toArray()
-        
+        if(isVisited) {
+            user = await db.collection('users').findOne({_id:new ObjectId(context.params.id)})
+            reviews = await db.collection('reviews').find({userId:context.params.id}).toArray()
+            favorites = await db.collection('favorites').find({userId:context.params.id}).toArray()
+        } else {
+            user = await db.collection('users').findOne({_id:new ObjectId(session?.user?.userId)})
+            reviews = await db.collection('reviews').find({userId:session?.user?.userId}).toArray()
+            favorites = await db.collection('favorites').find({userId:session?.user?.userId}).toArray()
+        }
+
         return {
             props: {
                 user: {
                     username: user.username,
                 },
                 favorites: JSON.parse(JSON.stringify(favorites)),
-                reviews:JSON.parse(JSON.stringify(reviews))
+                reviews:JSON.parse(JSON.stringify(reviews)),
+                visited:isVisited
             }
         }
 
