1500d75af4772b0b3166b95aca50ef23fd0a103b
Anton Migolko <migolkoanton@gmail.com>
2022-07-22 12:45:00 +0300

    disabled warning on build

--- next.config.js
+++ next.config.js
@@ -1,20 +1,23 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  "presets": ["next/babel"],
+  presets: ["next/babel"],
   reactStrictMode: false,
   webpack(config) {
     config.module.rules.push({
       test: /\.svg$/,
-      use: ['@svgr/webpack'],
+      use: ["@svgr/webpack"],
     });
     return config;
   },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
   images: {
-    domains: ['media.rawg.io','hatrabbits.com'],
+    domains: ["media.rawg.io", "hatrabbits.com"],
   },
   env: {
-    key:'e996863ffbd04374ac0586ec2bcadd55'
-  }
-}
+    key: "e996863ffbd04374ac0586ec2bcadd55",
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
