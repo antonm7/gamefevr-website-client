abd576b03c23349236bd592aa85c9ac460fa95f7
Anton Migolko <migolkoanton@gmail.com>
2022-08-12 19:31:04 +0300

    write review added functionallity

--- pages/api/game/action/review/writeReview.tsx
+++ pages/api/game/action/review/writeReview.tsx
@@ -1,97 +1,112 @@
-import { ObjectId } from "bson";
-import { NextApiRequest, NextApiResponse } from "next";
-import games_data_document from "../../../../../lib/functions/create/games_data";
-import clientPromise from "../../../../../lib/functions/mongodb";
-import { Review_Type } from "../../../../../types/schema";
+import { ObjectId } from 'bson'
+import { NextApiRequest, NextApiResponse } from 'next'
+import games_data_document from '../../../../../lib/functions/create/games_data'
+import clientPromise from '../../../../../lib/functions/mongodb'
+import { Review_Type } from '../../../../../types/schema'
 
 interface ExtendedNextApiRequest extends NextApiRequest {
   body: {
-    userId: string;
-    gameId: string;
-    rank: string;
-    text: string;
-  };
+    userId: string
+    gameId: string
+    rank: string
+    text: string
+  }
 }
 
 export default async function handler(
   req: ExtendedNextApiRequest,
   res: NextApiResponse
 ) {
-  if (req.method === "POST") {
-    let db = null;
-    const query = req.body;
-    let savedReview;
+  if (req.method === 'POST') {
+    let db = null
+    const query = req.body
+    let savedReview
     //initializing database
     try {
-      const client = await clientPromise;
-      db = client.db("gameFevr");
+      const client = await clientPromise
+      db = client.db('gameFevr')
     } catch (e) {
-      res.status(500).send({ error: "Unexpected error" });
-      return console.log("error on initializing database", e);
+      res.status(500).send({ error: 'Unexpected error' })
+      return console.log('error on initializing database', e)
     }
     //checks if game document exists
     try {
-      await games_data_document(req.body.gameId);
+      await games_data_document(req.body.gameId)
     } catch (e) {
-      res.status(500).send({ error: "Unexpected error" });
-      return console.log("error on games_data_document", e);
+      res.status(500).send({ error: 'Unexpected error' })
+      return console.log('error on games_data_document', e)
     }
     //saves the reviews inside own ranks collection
     try {
       const getData = await fetch(
         `https://api.rawg.io/api/games/${query.gameId}?key=0ffbdb925caf4b20987cd068aa43fd75`
-      );
-      const gameData = await getData.json();
+      )
+      const gameData = await getData.json()
+
+      const generateRank = (rank: string) => {
+        switch (rank) {
+          case 'waste_of_time':
+            return 'Waste Of Time'
+          case 'nuh':
+            return 'Nuh'
+          case 'good':
+            return 'Good'
+          case 'must':
+            return 'Must'
+          default:
+            return 'Good'
+        }
+      }
 
       const review: Review_Type = {
         userId: query.userId,
         gameId: query.gameId,
         game_name: gameData.name,
         game_image: gameData.background_image,
-        created_at: "time",
-        rank: query.rank,
+        created_at: 'time',
+        rank: generateRank(query.rank),
         text: query.text,
         likes: [],
         dislikes: [],
-      };
+      }
 
-      savedReview = await db.collection("reviews").insertOne(review);
+      savedReview = await db.collection('reviews').insertOne(review)
     } catch (e) {
-      res.status(500).send({ error: "Unexpected error" });
-      return console.log("error saving the review", e);
+      res.status(500).send({ error: 'Unexpected error' })
+      return console.log('error saving the review', e)
     }
     //updates user's document
     try {
       await db
-        .collection("users")
+        .collection('users')
         .updateOne(
           { _id: new ObjectId(query.userId) },
           { $push: { reviews: savedReview?.insertedId } }
-        );
+        )
     } catch (e) {
-      res.status(500).send({ error: "Unexpected error" });
-      return console.log("error on updating user ranks field");
+      res.status(500).send({ error: 'Unexpected error' })
+      return console.log('error on updating user ranks field')
     }
     //updates game data document
     try {
-      await db.collection("games_data").updateOne(
+      await db.collection('games_data').updateOne(
         { gameId: query.gameId },
         {
           $inc: { reviews: 1 },
         }
-      );
+      )
     } catch (e) {
-      res.status(500).send({ error: "Unexpected error" });
-      return console.log("error on updating games_data document", e);
+      res.status(500).send({ error: 'Unexpected error' })
+      return console.log('error on updating games_data document', e)
     }
     try {
       const review = await db
-        .collection("reviews")
-        .findOne({ _id: savedReview?.insertedId });
-      res.status(201).send({ error: null, review });
+        .collection('reviews')
+        .findOne({ _id: savedReview?.insertedId })
+      res.status(201).send({ error: null, review })
     } catch (e) {
-      res.status(500).send({ error: "Unexpected error" });
-      return console.log("error on updating games_data document", e);
+      res.status(500).send({ error: 'Unexpected error' })
+      return console.log('error on updating games_data document', e)
     }
   }
 }
