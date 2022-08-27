d3549824773e91bde5b3499feae0fd9a75693c8d
Anton Migolko <migolkoanton@gmail.com>
2022-08-27 12:47:16 +0300

    changed to ts the endpoint

---
+++ pages/api/explore/get/search.ts
@@ -0,0 +1,45 @@
+import { NextApiRequest, NextApiResponse } from 'next'
+import getRandomInt from '../../../../lib/functions/generateRandom'
+import { genres, parentConsoles } from '../../../../lib/staticData'
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  if (req.method === 'GET') {
+    const limit = 40
+    const page = Math.round(getRandomInt(0, 760000) / limit)
+    let filteredString = ''
+
+    const useOrNot = () => {
+      const num = Math.round(Math.random())
+      if (num === 0) {
+        return false
+      }
+      return true
+    }
+
+    if (useOrNot()) {
+      const consoles = parentConsoles
+      let consolesString = ''
+      const item = consoles[Math.floor(Math.random() * consoles.length)]
+      consolesString = consolesString.concat(`${item.id}`, '')
+      filteredString = filteredString.concat(`&consoles=${consolesString}`)
+    }
+    //add more
+    if (useOrNot()) {
+      const genresData = genres
+      let genresString = ''
+      const item = genresData[Math.floor(Math.random() * genresData.length)]
+      genresString = genresString.concat(`${item.id}`, '')
+      filteredString = filteredString.concat(`&platforms=${genresString}`)
+    }
+
+    const getData = await fetch(
+      `https://api.rawg.io/api/games?key=0ffbdb925caf4b20987cd068aa43fd75&page=${page}&page_size=${limit}`
+    )
+
+    const games = await getData.json()
+    return res.status(200).send(games)
+  }
+}
