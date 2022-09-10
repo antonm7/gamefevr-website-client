95ff666f0dda0c48e8eac73c3cd5e792a1e23f16
Anton Migolko <migolkoanton@gmail.com>
2022-09-10 14:08:41 +0300

    Changed to react memo component

--- components/animations/Car.tsx
+++ components/animations/Car.tsx
@@ -1,22 +1,24 @@
-import Lottie from "react-lottie";
-import * as animationData from "../../public/animations/1920.json";
+import React from 'react'
+import Lottie from 'react-lottie'
+import * as animationData from '../../public/animations/1920.json'
 
-export default function Car() {
+const Car = React.memo(() => {
   const defaultOptions = {
     loop: true,
     autoplay: true,
     animationData: animationData,
     rendererSettings: {
-      preserveAspectRatio: "xMaxYMax meet", // Supports the same options as the svg element's preserveAspectRatio property
+      preserveAspectRatio: 'xMaxYMax meet', // Supports the same options as the svg element's preserveAspectRatio property
       clearCanvas: false,
       hideOnTransparent: true, //Boolean, only svg renderer, hides elements when opacity reaches 0 (defaults to true)
-      className: "some-css-class-name",
+      className: 'some-css-class-name',
     },
-  };
+  }
   return (
     <Lottie
       options={defaultOptions}
-      style={{ zIndex: 0, position: "absolute", bottom: 0 }}
+      style={{ zIndex: 0, position: 'absolute', bottom: 0 }}
     ></Lottie>
-  );
-}
+  )
+})
+export default Car
--- components/animations/Signup.tsx
+++ components/animations/Signup.tsx
@@ -1,22 +1,25 @@
-import Lottie from "react-lottie";
-import * as animationData from "../../public/animations/signup.json";
+import Lottie from 'react-lottie'
+import * as animationData from '../../public/animations/signup.json'
+import React from 'react'
 
-export default function SignupAnimation() {
+const SignupAnimation = React.memo(() => {
   const defaultOptions = {
     loop: true,
     autoplay: true,
     animationData: animationData,
     rendererSettings: {
-      preserveAspectRatio: "xMaxYMax meet", // Supports the same options as the svg element's preserveAspectRatio property
+      preserveAspectRatio: 'xMaxYMax meet', // Supports the same options as the svg element's preserveAspectRatio property
       clearCanvas: false,
       hideOnTransparent: true, //Boolean, only svg renderer, hides elements when opacity reaches 0 (defaults to true)
-      className: "some-css-class-name",
+      className: 'some-css-class-name',
     },
-  };
+  }
   return (
     <Lottie
       options={defaultOptions}
-      style={{ zIndex: 0, position: "absolute", bottom: 0 }}
+      style={{ zIndex: 0, position: 'absolute', bottom: 0 }}
     ></Lottie>
-  );
-}
+  )
+})
+
+export default SignupAnimation
