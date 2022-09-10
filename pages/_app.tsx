b4cf6c7a130bc1f93256df6a127bbe3e3648fe7b
Anton Migolko <migolkoanton@gmail.com>
2022-09-10 14:11:11 +0300

    listening to route query change, and updated the filters state

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -8,15 +8,22 @@ import { SessionProvider } from 'next-auth/react'
 import { useEffect } from 'react'
 import { useRouter } from 'next/router'
 import Progress from '../components/progress/Progress'
-import { useGlobalError, useProgressStore } from '../store'
+import {
+  useFiltersStore,
+  useGlobalError,
+  useProgressStore,
+  useStore,
+} from '../store'
 import GlobalError from '../components/common/GlobalError/Index'
 
 function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
+  const changeGameName = useStore((state) => state.changeGameName)
+  const filtersStore = useFiltersStore((state) => state)
   const setIsAnimating = useProgressStore((state) => state.setIsAnimating)
   const isAnimating = useProgressStore((state) => state.isAnimating)
   const isVisible = useGlobalError((state) => state.isVisible)
   const type = useGlobalError((state) => state.type)
-  const router = useRouter()
+  const router: any = useRouter()
 
   useEffect(() => {
     const handleStart = () => {
@@ -45,17 +52,52 @@ function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
 
   useEffect(() => {
     if (router.pathname === '/search') {
-      // console.log('changeddedede', router.query)
-      // filtersStore.setConsoles(
-      //   router.query.consoles ? router.query.consoles : []
-      // )
-      // filtersStore.setGenres(router.query.genres ? router.query.genres : [])
-      // filtersStore.setYearRange(
-      //   router.query.yearRange ? router.query.yearRange : []
-      // )
-      // changeGameName(router.query.search ? router.query.search : '')
+      //updateing consoles
+      const updatedConsoles = (): string[] => {
+        if (!router.query.consoles) {
+          return []
+        } else if (typeof router.query.consoles === 'string') {
+          return [router.query.consoles]
+        } else {
+          return router.query.consoles
+        }
+      }
+      filtersStore.setConsoles(updatedConsoles())
+
+      //updating genres
+      const updatedGenres = (): string[] => {
+        if (!router.query.genres) {
+          return []
+        } else if (typeof router.query.genres === 'string') {
+          return [router.query.genres]
+        } else {
+          return router.query.genres
+        }
+      }
+      filtersStore.setGenres(updatedGenres())
+
+      //updating years
+      const updatedYears = (): number[] => {
+        if (router.query.yearRange && !Array.isArray(router.query.yearRange)) {
+          return [1990, 2023]
+        }
+        if (!router.query.yearRange || router.query.yearRange.length !== 2) {
+          return [1990, 2023]
+        } else {
+          return router.query.yearRange
+        }
+      }
+      filtersStore.setYearRange(updatedYears())
+
+      changeGameName(router.query.search ? router.query.search : '')
     }
-  }, [router.query])
+  }, [
+    router.query,
+    // router.query.yearRange,
+    // router.query.consoles,
+    // router.query.search,
+    // router.query.genres,
+  ])
 
   return (
     <>
