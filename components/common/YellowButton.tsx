0f91af740b5022191280450f5b39862fd815c840
antonm7 <migolkoanton@gmail.com>
2022-10-09 20:00:23 +0300

    props cahnges

--- components/common/YellowButton.tsx
+++ components/common/YellowButton.tsx
@@ -2,18 +2,30 @@ interface Props {
   title: string
   active?: boolean
   onClick: () => void
+  complete?: boolean
+  completeTitle?: string
 }
 
-export default function YellowButton({ title, active, onClick }: Props) {
-  return (
-    <button
-      className={`w-full h-12 bg-specialYellow rounded-lg text-white text-lg font-normal ${
-        !active && active !== undefined ? 'opacity-50 cursor-not-allowed' : ''
-      }`}
+export default function YellowButton({ title, active, onClick, complete, completeTitle }: Props) {
+  if (complete) {
+    return (<button
+      className={`w-full h-12 bg-[#50c878] rounded-lg text-white text-lg font-normal ${!active && active !== undefined ? 'opacity-50 cursor-not-allowed' : ''
+        }`}
       onClick={onClick}
-      title={title}
+      title={completeTitle}
     >
-      {title}
-    </button>
-  )
+      {completeTitle}
+    </button>)
+  } else {
+    return (
+      <button
+        className={`w-full h-12 bg-specialYellow rounded-lg text-white text-lg font-normal ${!active && active !== undefined ? 'opacity-50 cursor-not-allowed' : ''
+          }`}
+        onClick={onClick}
+        title={title}
+      >
+        {title}
+      </button>
+    )
+  }
 }
