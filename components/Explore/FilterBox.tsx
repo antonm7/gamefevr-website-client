9c42a96158d03d033a7ff522b0d2b65d259f90d2
Anton Migolko <migolkoanton@gmail.com>
2022-10-22 14:03:05 +0300

    selecting filters and mange states works

--- components/Explore/FilterBox.tsx
+++ components/Explore/FilterBox.tsx
@@ -6,7 +6,9 @@ interface Props {
 export default function FilterBox({ name, onClick, isSelected }: Props) {
   return (
     <div
-      className="opacity-70 hover:opacity-100 cursor-pointer h-auto font-semibold whitespace-nowrap w-auto bg-cool-blue m-3 p-2 overflow-hidden text-center rounded-lg text-main-blue"
+      className={`${
+        isSelected ? 'opacity-100' : 'opacity-70'
+      } hover:opacity-100 cursor-pointer h-auto font-semibold whitespace-nowrap w-auto bg-cool-blue m-3 p-2 overflow-hidden text-center rounded-lg text-main-blue`}
       onClick={onClick}
     >
       <p className="text-sm">{name}</p>
--- pages/explore/index.tsx
+++ pages/explore/index.tsx
@@ -1,16 +1,9 @@
-import { faArrowDown, faArrowUp } from '@fortawesome/free-solid-svg-icons'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import axios from 'axios'
-import { ObjectId } from 'bson'
-import { GetServerSideProps, GetServerSidePropsContext } from 'next'
-import { getSession } from 'next-auth/react'
-import { ReactElement, useCallback, useEffect, useRef, useState } from 'react'
-import SmallLoader from '../../components/common/SmallLoader'
-import ExploreScroll from '../../components/Explore/ExploreScroll'
-import FilterBox from '../../components/Explore/FilterBox'
+import { GetServerSideProps } from 'next'
+import { useEffect, useState } from 'react'
 import SearchLayout from '../../components/layout/SearchLayout'
-import { ElementDescription, ShortGame } from '../../types'
+import { ElementDescription } from '../../types'
 import { genres, parentConsoles } from '../../lib/staticData'
+import FiltersRow from '../../components/Explore/FiltersRow'
 
 interface Props {
   random_genres: ElementDescription[]
@@ -19,10 +12,6 @@ interface Props {
 }
 
 export default function Index({ random_genres, random_platforms }: Props) {
-  const [selectedGenres, setSelectedGenres] = useState<string[]>([])
-  const [selectedPlatforms, setSelectedPlatforms] = useState<string[]>([])
-  const [selectedYears, setSelectedYears] = useState<string[]>([])
-
   const [genres, setGenres] = useState<ElementDescription[]>([])
   const [platforms, setPlatforms] = useState<ElementDescription[]>([])
 
@@ -40,41 +29,7 @@ export default function Index({ random_genres, random_platforms }: Props) {
   useEffect(() => {
     setGenres(random_genres)
     setPlatforms(random_platforms)
-  }, [random_genres, random_platforms])
-
-  const GenerateOrder = (
-    arr1: ElementDescription[],
-    arr2: ElementDescription[]
-  ): ReactElement => {
-    function shuffle(array: ElementDescription[]) {
-      let currentIndex = array.length,
-        randomIndex
-
-      while (currentIndex != 0) {
-        randomIndex = Math.floor(Math.random() * currentIndex)
-        currentIndex--
-        ;[array[currentIndex], array[randomIndex]] = [
-          array[randomIndex],
-          array[currentIndex],
-        ]
-      }
-
-      return array
-    }
-
-    return (
-      <>
-        {shuffle([...arr1, ...arr2]).map((g) => (
-          <FilterBox
-            name={g.name}
-            key={g.name}
-            onClick={() => setSelectedGenres((arr) => [...arr, g.id])}
-            isSelected={selectedGenres.includes(g.id)}
-          />
-        ))}
-      </>
-    )
-  }
+  }, [])
 
   return (
     <SearchLayout>
@@ -85,30 +40,21 @@ export default function Index({ random_genres, random_platforms }: Props) {
         >
           Start Exploring Games New Games!
         </p>
-        <div className="flex flex-wrap pt-4 ">
-          {GenerateOrder(genres, platforms)}
-          <FilterBox
-            name="New Games"
-            onClick={() =>
-              selectedYears.includes('new_games')
-                ? setSelectedYears(
-                    selectedYears.filter((y) => y !== 'new_games')
-                  )
-                : setSelectedYears([...selectedYears, 'new_games'])
-            }
-            isSelected={selectedYears.includes('new_games')}
-          />
-        </div>
+        <FiltersRow genres={genres} platforms={platforms} />
       </div>
     </SearchLayout>
   )
 }
 
+interface ElementExtends extends ElementDescription {
+  type?: string
+}
+
 export async function getServerSideProps(context: GetServerSideProps) {
   //fetch random genres
 
-  const random_genres: ElementDescription[] = []
-  const random_platforms: ElementDescription[] = []
+  const random_genres: ElementExtends[] = []
+  const random_platforms: ElementExtends[] = []
 
   const shuffle = (
     currArr: ElementDescription[],
@@ -138,6 +84,22 @@ export async function getServerSideProps(context: GetServerSideProps) {
     shuffle(random_genres, genres, 'genres')
     shuffle(random_platforms, parentConsoles, 'platforms')
   }
+
+  for (const key in random_genres) {
+    random_genres[key] = {
+      ...random_genres[key],
+      type: 'genres',
+    }
+  }
+
+  for (const key in random_platforms) {
+    random_platforms[key] = {
+      ...random_platforms[key],
+      type: 'platforms',
+    }
+  }
+
+  console.log(random_genres, random_platforms)
 
   return {
     props: {
