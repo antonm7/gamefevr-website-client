30917d21c1a7fa6b26e52482649c2cbb0c8efbf7
Anton Migolko <migolkoanton@gmail.com>
2022-10-22 19:51:34 +0300

    fetchinf games and showing them now works

--- pages/explore/index.tsx
+++ pages/explore/index.tsx
@@ -1,17 +1,26 @@
 import { GetServerSideProps } from 'next'
 import { useEffect, useState } from 'react'
 import SearchLayout from '../../components/layout/SearchLayout'
-import { ElementDescription } from '../../types'
+import { DetailedGame, ElementDescription, ShortGame } from '../../types'
 import { genres, parentConsoles } from '../../lib/staticData'
 import FiltersRow from '../../components/Explore/FiltersRow'
+import getRandomInt from '../../lib/functions/generateRandom'
+import clientPromise from '../../lib/functions/mongodb'
+import { getSession } from 'next-auth/react'
+import { games_data } from '../../types/schema'
+import SmallGameBox from '../../components/SmallGameBox'
 
 interface Props {
   random_genres: ElementDescription[]
   random_platforms: ElementDescription[]
-  explored: number
+  games: ShortGame[]
 }
 
-export default function Index({ random_genres, random_platforms }: Props) {
+export default function Index({
+  random_genres,
+  random_platforms,
+  games,
+}: Props) {
   const [genres, setGenres] = useState<ElementDescription[]>([])
   const [platforms, setPlatforms] = useState<ElementDescription[]>([])
 
@@ -29,23 +38,30 @@ export default function Index({ random_genres, random_platforms }: Props) {
   useEffect(() => {
     setGenres(random_genres)
     setPlatforms(random_platforms)
+    console.log(games)
   }, [])
 
   return (
     <SearchLayout>
       <div className="px-44 pb-10 py-10">
-        <div id="explore_wrapper" className='flex h-auto'>
+        <div id="explore_wrapper" className="flex h-auto">
           <p
             id="we_found_title"
             className="explore_title we_found_padding font-bold text-white text-4xl lea"
           >
             Start Exploring Games New Games!
           </p>
-          <div id='explore_helper'>
-            ?
-          </div>
         </div>
+        <p className="pt-4 text-white opacity-60 text-sm font-semibold">
+          Discover new games based on your recent changes or use some filters
+          below
+        </p>
         <FiltersRow genres={genres} platforms={platforms} />
+        <div id="games_wrapper" className="flex flex-wrap justify-center pt-12">
+          {games.map((game: ShortGame, index: number) => (
+            <SmallGameBox key={index} game={game} />
+          ))}
+        </div>
       </div>
     </SearchLayout>
   )
@@ -55,7 +71,7 @@ interface ElementExtends extends ElementDescription {
   type?: string
 }
 
-export async function getServerSideProps(context: GetServerSideProps) {
+export async function getServerSideProps(context: any) {
   //fetch random genres
 
   const random_genres: ElementExtends[] = []
@@ -104,12 +120,78 @@ export async function getServerSideProps(context: GetServerSideProps) {
     }
   }
 
-  console.log(random_genres, random_platforms)
+  try {
+    const limit = 25
+    const page = Math.round(getRandomInt(2, 100) / limit)
+    let filteredString = ''
 
-  return {
-    props: {
-      random_genres,
-      random_platforms,
-    },
+    const useOrNot = () => {
+      const num = Math.round(Math.random())
+      if (num === 0) {
+        return false
+      }
+      return true
+    }
+
+    if (useOrNot()) {
+      const consoles = parentConsoles
+      let consolesString = ''
+      const item = consoles[Math.floor(Math.random() * consoles.length)]
+      consolesString = consolesString.concat(`${item.id}`, '')
+      filteredString = filteredString.concat(`&consoles=${consolesString}`)
+    }
+    //add more
+    if (useOrNot()) {
+      const genresData = genres
+      let genresString = ''
+      const item = genresData[Math.floor(Math.random() * genresData.length)]
+      genresString = genresString.concat(`${item.id}`, '')
+      filteredString = filteredString.concat(`&platforms=${genresString}`)
+    }
+
+    const getData: any = await fetch(
+      `https://api.rawg.io/api/games?key=39a2bd3750804b5a82669025ed9986a8&page=${page}&page_size=${limit}${filteredString}`
+    )
+
+    const result = await getData.json()
+
+    // const games: ShortGame[] = []
+    // const data = await getData.json().results
+
+    const games = []
+
+    if (!result) {
+      const data: any = await fetch(
+        `https://api.rawg.io/api/games?key=39a2bd3750804b5a82669025ed9986a8&page=1&page_size=25`
+      )
+      const secondResult = await data.json()
+      console.log(secondResult)
+      console.log(secondResult.results.length)
+      games.push(...secondResult.results)
+    } else {
+      console.log(result.results.length)
+      games.push(...result.results)
+    }
+
+    console.log(games)
+
+    return {
+      props: {
+        random_genres,
+        random_platforms,
+        games,
+        error: null,
+      },
+    }
+  } catch (e) {
+    console.log('error on explore page', e)
+    return {
+      props: {
+        random_genres,
+        random_platforms,
+        games: null,
+        error: 'Oops, Unexpected Error',
+      },
+    }
   }
 }
