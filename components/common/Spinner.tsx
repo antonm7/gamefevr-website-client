3714e8354412731c23bd5cc8f469e64126b0169d
antonm7 <migolkoanton@gmail.com>
2022-11-25 19:41:21 +0200

    different loader added

---
+++ components/common/Spinner.tsx
@@ -0,0 +1,13 @@
+import { SpinnerCircular } from "spinners-react";
+
+export default function Spinner() {
+    return (
+        <SpinnerCircular
+            className="mx-auto"
+            size={16}
+            thickness={117}
+            speed={85}
+            color="#0c203a"
+        />
+    )
+}
