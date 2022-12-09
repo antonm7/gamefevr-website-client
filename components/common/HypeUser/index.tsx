41357d4183b5426b06ad305f2bdd0eb534f92f0f
antonm7 <migolkoanton@gmail.com>
2022-12-09 17:57:50 +0200

    checking if user hyped on server side

--- components/common/HypeUser/index.tsx
+++ components/common/HypeUser/index.tsx
@@ -1,46 +1,45 @@
 import { faArrowUp } from "@fortawesome/free-solid-svg-icons"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import axios from "axios"
+import { useSession } from "next-auth/react"
 import { useRouter } from "next/router"
-import { useEffect, useState } from "react"
-
-
+import { useState } from "react"
+import { useGlobalError } from "../../../store"
 
 export default function HypeUser() {
     const router = useRouter()
     const userId = router.query.id
+    const session = useSession()
     const [isHyped, setIsHyped] = useState<boolean>(false)
+    const globalErrorState = useGlobalError(state => state)
 
-    const is_already_hyped: () => Promise<void> = async () => {
+    const hypeUserMethod = async (): Promise<void> => {
         try {
-            const req = await axios.get(`/api/user/get/isHyped?userId=${userId}`)
-            if (req.status === 200) {
-                if (req.data.error) throw new Error(req.data.error)
-                if (req.data.isHyped) {
-                    setIsHyped(true)
-                } else {
-                    setIsHyped(false)
-                }
-            } else {
+            const req = await axios.post('/api/user/action/hype', {
+                gets_hype: userId,
+                sends_hype: session.data?.user.userId
+            })
+            if (req.status !== 201 && req.status !== 200) {
                 throw new Error()
+            } else {
+                setIsHyped(true)
+                globalErrorState.setType('success')
+                globalErrorState.setText(`You hyped the user!`)
+                globalErrorState.setIsVisible(true)
             }
         } catch (e) {
-            console.log('error getting hyped', e)
-            setIsHyped(false)
+            globalErrorState.setType('error')
+            globalErrorState.setText(`oops, could'nt hype the user...`)
+            globalErrorState.setIsVisible(true)
         }
     }
-
-    useEffect(() => {
-        is_already_hyped()
-    }, [])
 
     if (isHyped) return null
 
     return (
-        <div className={`pl-4 cursor-pointer`}>
-            <div className="h-12 flex flex-col justify-center items-center  mr-4">
-                <FontAwesomeIcon icon={faArrowUp} className="font-semibold text-specialYellow" />
-                <p className='text-specialYellow font-medium select-none'>hype</p>
+        <div onClick={() => hypeUserMethod()}>
+            <div className="h-8 w-8 flex flex-col justify-center items-center overflow-hidden rounded-full" style={{ border: "1px solid #38b6cc" }}>
+                <FontAwesomeIcon icon={faArrowUp} className="font-semibold text-cool-blue cursor-pointer h-4" />
             </div>
         </div >
     )
