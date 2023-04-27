12b548c9539a81d8f57d42df3f1d8ef45d712dbc
antonm7 <migolkoanton@gmail.com>
2023-04-27 10:39:22 +0200

    config changes

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,5 +1,8 @@
 module.exports = {
     "rules": {
         "@typescript-eslint/no-explicit-any": "off"
-    }
+    },
+    parser: "@babel/eslint-parser",
+    requireConfigFile: false,
+
 };
--- next.config.js
+++ next.config.js
@@ -25,8 +25,8 @@ const nextConfig = {
     SENDGRID_API_KEY: process.env.SENDGRID_API_KEY,
     MONGODB_URI: process.env.MONGODB_URI,
     FETCH_GAMES_KEY_GENERAL1: process.env.FETCH_GAMES_KEY_GENERAL1,
-    FETCH_GAMES_KEY_GENERAL2: process.env.FETCH_GAMES_KEY_GENERAL2,
-    BUILD_GAMES_KEY: process.env.BUILD_GAMES_KEY
+    // FETCH_GAMES_KEY_GENERAL2: process.env.FETCH_GAMES_KEY_GENERAL2,
+    // BUILD_GAMES_KEY: process.env.BUILD_GAMES_KEY
   }
 };
 // eslint-disable-next-line no-undef
