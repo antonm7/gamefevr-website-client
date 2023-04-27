9bb647553a2e508235f28d257a431dd6f92d1a40
antonm7 <migolkoanton@gmail.com>
2023-04-27 10:42:42 +0200

    loader and icons changed to fontAwesome

--- components/GamePage/Responsive/Footer/Lower1200/index.tsx
+++ components/GamePage/Responsive/Footer/Lower1200/index.tsx
@@ -6,16 +6,21 @@ import Screenshots from "../../../Screenshots";
 import styles from './index.module.scss';
 import Image from "next/image";
 import { useRef } from "react";
+import SmallLoader from "../../../../common/SmallLoader";
+import { IconProp } from "@fortawesome/fontawesome-svg-core";
+import { faArrowLeft, faArrowRight } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 type Props = {
     screenshots: Short_Screenshot[]
     navigateAuth: () => void
     deleteReview: (id: ObjectId | undefined) => void
+    screenshots_loader: boolean
     reviews: Review_Type[]
     sliderRef: any
 }
 
-export default function Lower1200({ reviews, navigateAuth, deleteReview, screenshots }: Props) {
+export default function Lower1200({ reviews, navigateAuth, deleteReview, screenshots, screenshots_loader }: Props) {
     const sliderRef = useRef<any>(null)
     return (
         <div>
@@ -23,26 +28,22 @@ export default function Lower1200({ reviews, navigateAuth, deleteReview, screens
                 <div className="absolute z-20 h-full w-[600px] bg-darkIndigo" id={styles.blueBox} >
                     <div className="pt-[23rem] pl-28">
                         <div id={styles.arrows_wrapper} className="flex items-center bottom-0">
-                            <Image
+                            <FontAwesomeIcon className="h-6 text-white"
+                                icon={faArrowLeft as IconProp}
                                 onClick={() => sliderRef.current.slickPrev()}
-                                src={'/icons/arrow_left.svg'}
-                                width={25}
-                                height={18}
-                                alt="arrow-left"
                             />
                             <div className="ml-8 bg-white flex items-center justify-center py-2 px-4 rounded-lg">
-                                <Image
+                                <FontAwesomeIcon className="h-6 text-inputBg"
+                                    icon={faArrowRight as IconProp}
                                     onClick={() => sliderRef.current.slickNext()}
-                                    src={'/icons/arrow_right.svg'}
-                                    width={25}
-                                    height={18}
-                                    alt="arrow-right"
                                 />
                             </div>
                         </div>
                     </div>
                 </div>
-                <Screenshots sliderRef={sliderRef} lower1200={true} images={screenshots} />
+                {screenshots_loader ? <SmallLoader /> :
+                    <Screenshots sliderRef={sliderRef} lower1200={true} images={screenshots} />
+                }
             </div>
             <div className="mt-32">
                 <ReviewsSlider
