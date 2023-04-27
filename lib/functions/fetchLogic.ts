dd816746879af74da08dea8ba9dc78a997f38c08
antonm7 <migolkoanton@gmail.com>
2023-04-27 10:44:12 +0200

    changed wretchAction logic with errors

--- lib/functions/fetchLogic.ts
+++ lib/functions/fetchLogic.ts
@@ -12,16 +12,53 @@ export function wretchWrapper(url: string, request_name: string): Promise<promis
         .json(data => data)
 }
 
+interface ErrorResponse {
+    message: string;
+    statusCode: number;
+}
+
+
+interface ErrorResponse {
+    message: string;
+    statusCode: number;
+}
+
 export function wretchAction(url: string, body: unknown): Promise<promiseSettledResponse> {
     return wretch(url)
         .post({ body })
-        .badRequest(() => { throw new Error() })
-        .notFound(() => { throw new Error() })
-        .unauthorized(() => { throw new Error() })
-        .internalError(() => { throw new Error() })
+        .badRequest((e) => {
+            const error: ErrorResponse = {
+                message: e.message,
+                statusCode: e.status,
+            };
+            throw error;
+        })
+        .notFound((e) => {
+            const error: ErrorResponse = {
+                message: e.message,
+                statusCode: e.status,
+            };
+            throw error;
+        })
+        .unauthorized((e) => {
+            const error: ErrorResponse = {
+                message: e.message,
+                statusCode: e.status,
+            };
+            throw error;
+        })
+        .internalError((e) => {
+            const error: ErrorResponse = {
+                message: e.message,
+                statusCode: e.status,
+            };
+            throw error;
+        })
         .res(response => response.json())
+        .catch(error => {
+            throw error
+        });
 }
-
 
 export function promiseHandler(results: promiseSettledResponse[]):
     promiseSettledResponse[] {
